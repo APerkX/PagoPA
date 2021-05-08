32c17d48bedabeeccd7a62956fe064c12482c9b9
AndreaPerquoti <ap92@hotmail.it>
2021-05-08 19:29:33 +0200

    Startup commit

---
+++ src/main/java/it/perk/pagopa/PagoPAinterviewApplication.java
@@ -0,0 +1,13 @@
+package it.perk.pagopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoPAinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagoPAinterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/perk/pagopa/PagoPAinterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package it.perk.pagopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoPAinterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
